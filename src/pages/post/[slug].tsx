b78d0194b3030b25e609f41936c69787a85e1c16
luan <barkon32@tuta.io>
2022-10-25 15:59:43 -0300

    Getting post page elements ready

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { ReactElement } from 'react';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +28,28 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post(): ReactElement {
+  return (
+    <>
+      <Head>
+        <title>Titulo</title>
+      </Head>
+
+      <div className={styles.banner}>
+        <img
+          src="https://images.unsplash.com/photo-1595864968156-b643d9028611?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5pZ2h0JTIwcGhvdG9ncmFwaHl8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+      </div>
+      <div className={styles.postsList}>
+        <p>Content goes here</p>
+      </div>
+      <div className={styles.loadMoreButton}>
+        <button type="button">Carregar mais posts</button>
+      </div>
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient({});
