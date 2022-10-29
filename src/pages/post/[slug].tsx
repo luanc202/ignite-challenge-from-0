4b73a5ae63c83bbd2f7d8a046a8b16b103d0cb90
Luan <luan@luan-desktop>
2022-10-29 16:38:31 -0300

    Slug content loading finished

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -3,9 +3,11 @@ import Head from 'next/head';
 import { ReactElement } from 'react';
 import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
+import * as prismicH from '@prismicio/helpers';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
+import formatDateToBR from '../../utils/date-formatter';
 import styles from './post.module.scss';
 
 interface Post {
@@ -29,30 +31,27 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post(): ReactElement {
+export default function Post({ post }: PostProps): ReactElement {
   return (
     <>
       <Head>
-        <title>Titulo</title>
+        <title>{post.data.title}</title>
       </Head>
 
       <div className={styles.banner}>
-        <img
-          src="https://images.unsplash.com/photo-1595864968156-b643d9028611?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5pZ2h0JTIwcGhvdG9ncmFwaHl8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-          alt=""
-        />
+        <img src={post.data.banner.url} alt="" />
       </div>
       <div className={commonStyles.container}>
         <div className={styles.postHeader}>
-          <h1>Criando um app CRA do zero</h1>
+          <h1>{post.data.title}</h1>
           <div>
             <div>
               <FiCalendar size={20} />
-              <time>15 Mar 2021</time>
+              <time>{formatDateToBR(post.first_publication_date)}</time>
             </div>
             <div>
               <FiUser size={20} />
-              <span>Joseph Oliveira</span>
+              <span>{post.data.author}</span>
             </div>
             <div>
               <FiClock size={20} />
@@ -61,32 +60,44 @@ export default function Post(): ReactElement {
           </div>
         </div>
         <div className={styles.postContent}>
-          <h1>Protein et varius</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos alias
-            laboriosam accusamus iste ipsa labore eveniet tempora laborum
-            tempore blanditiis voluptatum aliquid minima recusandae quod, fugit
-            iusto earum quibusdam? Adipisci dolores deleniti repellat earum
-            reprehenderit excepturi asperiores voluptate, exercitationem,
-            numquam eaque totam consectetur natus consequatur suscipit saepe
-            neque nisi delectus?
-          </p>
+          {post.data.content.map(content => (
+            <>
+              <h1 key={content.heading}>{content.heading}</h1>
+              {content.body.map(body => (
+                <p>{body.text}</p>
+              ))}
+            </>
+          ))}
         </div>
       </div>
     </>
   );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+export const getStaticPaths = async (): Promise<unknown> => {
+  const prismic = getPrismicClient({});
+  const posts = await prismic.getByType('post');
 
-//   // TODO
-// };
+  const paths = posts.results.map(post => ({
+    params: { slug: post.uid },
+  }));
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+  return { paths, fallback: true };
+};
 
-//   // TODO
-// };
+export const getStaticProps = async ({ params }): Promise<unknown> => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient({});
+
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  console.log(response.data);
+  console.log(response.data.content[0]);
+
+  return {
+    props: {
+      post: response,
+    },
+  };
+};
