77475705cb4f7db2c04333910e36c786e4d2aecb
Luan <luan@luan-desktop>
2022-10-31 14:19:29 -0300

    Now using more appropriate format

--- src/utils/date-formatter.ts
+++ src/utils/date-formatter.ts
@@ -2,5 +2,5 @@ import { ptBR } from 'date-fns/locale';
 import { format } from 'date-fns';
 
 export default function formatDateToBR(date: string): string {
-  return format(new Date(date), 'dd LLL y', { locale: ptBR });
+  return format(new Date(date), 'dd MMM y', { locale: ptBR });
 }
